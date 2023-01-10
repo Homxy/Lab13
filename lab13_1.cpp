71230809f7167d7f823d113de557975f7a64c8bb
Homxy <120322865+Homxy@users.noreply.github.com>
2023-01-10 15:15:09 +0700

    Create lab13_1.cpp

---
+++ lab13_1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include <ctime> 
+using namespace std;
+
+long long int fibonacci(long long int);
+
+int main(){
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+
+
+long long int fibonacci(long long int x){
+    if(x>1){
+        x = fibonacci(x-1) + fibonacci(x-2);
+        return x;
+    }
+    else{
+        return x;
+    }
+}
+
